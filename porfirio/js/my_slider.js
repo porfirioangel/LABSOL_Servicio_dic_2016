44c4aef61e1158cd14cb5f36c0be1d599986c486
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2016-12-16 11:07:40 -0600

    Ya funciona mi implementación del slider

---
+++ porfirio/js/my_slider.js
@@ -0,0 +1,29 @@
+/**
+ * Created by porfirio on 12/15/16.
+ */
+
+$(document).ready(function () {
+    var slideIndex = 1;
+    var totalSlides = 4;
+    var slider = $("#slider_labsol").responsiveSlides({
+        auto: true,
+        nav: true,
+        pause: true,
+        speed: 500,
+        namespace: "callbacks",
+        before: function () {
+            var mod = (slideIndex + 1) % totalSlides;
+            slideIndex = mod == 0 ? totalSlides : mod;
+            $('.events').append("<li>" + slideIndex + "</li>");
+            if (slideIndex == 4) {
+                $('#slider_labsol').trigger('mouseenter');
+            } else {
+                $('#slider_labsol').trigger('mouseleave');
+            }
+        },
+        after: function () {
+
+        }
+    });
+    $('.events').append("<li>" + slideIndex + "</li>");
+});
